d00774be93956f9da3849f059edcd612b384fe76
danghieu1407 <danghieu140701@gmail.com>
2024-07-03 21:53:09 +0700

    add taiwil css

--- managemoney/src/App.vue
+++ managemoney/src/App.vue
@@ -1,23 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Welcome to manage money!" />
-
-      <nav>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/register">About</RouterLink>
-      </nav>
+    <div class="h-fit w-fit">
+        <RouterView />
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
